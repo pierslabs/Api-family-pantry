d5388f56f496f10ee5c807b2677e38b4ac522f47
pierslabs <pedrolosasp2gmail.com>
2022-03-14 19:36:59 +0100

    change signup error message

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -10,7 +10,7 @@ export const signUp = async (req, res) => {
   const findUSer = await User.findOne({ email });
 
   if (findUSer) {
-    res.status(400).json("El usuario ya existe");
+    res.status(400).json({ status: false, data: "El usuario ya existe" });
   }
 
   const salt = await bcrypt.genSalt(10);

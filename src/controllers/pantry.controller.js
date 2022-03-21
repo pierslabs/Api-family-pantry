6f50fb0c00d9cfe0728538414aa69c3487a1f422
pierslabs <pedrolosasp2gmail.com>
2022-03-21 17:33:55 +0100

    upload getpantry

--- src/controllers/pantry.controller.js
+++ src/controllers/pantry.controller.js
@@ -29,12 +29,12 @@ export const getPantry = async (req, res) => {
 export const getPantrys = async (req, res) => {
   const user = decodeToken(req);
 
-  try {
-    const pantrys = await Pantry.find({ user_id: user.id });
-    res.status(200).json(pantrys);
-  } catch (error) {
-    console.log(error);
+  const pantrys = await Pantry.find({ user_id: user.id });
+
+  if (!pantrys) {
+    res.json(400).json({ status: false, data: "No existen cestas" });
   }
+  res.status(200).json({ status: true, data: pantrys });
 };
 
 export const createPantry = async (req, res) => {

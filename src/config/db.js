9c3798b2c590c3d4f3259d8c07f3d62805a64931
Pedro Losas <pedrolosasp@gmail.com>
2024-03-28 13:03:48 +0100

    update mongo

--- src/config/db.js
+++ src/config/db.js
@@ -1,12 +1,16 @@
-import mongoose from "mongoose";
-import dotenv from "dotenv";
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 
-dotenv.config({ path: "variables.env" });
+dotenv.config({ path: 'variables.env' });
 
 const dbConnect = async () => {
   const { connect } = mongoose;
   try {
-    const db = await connect(process.env.DB_MONGO);
+    mongoose.set('strictQuery', false);
+    const db = await connect(process.env.DB_MONGO, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
     console.log(`Connect DB ${db.connection.db.databaseName}`);
   } catch (error) {
     console.log(error);

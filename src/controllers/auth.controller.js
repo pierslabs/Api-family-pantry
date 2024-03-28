b3117b4b24a95eccc92afcf47a0a5953183131ac
Pedro Losas <pedrolosasp@gmail.com>
2024-03-28 13:27:14 +0100

    update mongo

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,16 +1,18 @@
-import User from "../models/user.js";
-import bcrypt from "bcryptjs";
-import { createToken } from "../helper/jwt.js";
-import dotenv from "dotenv";
+import User from '../models/user.js';
+import bcrypt from 'bcryptjs';
+import { createToken } from '../helper/jwt.js';
+import dotenv from 'dotenv';
 
-dotenv.config({ path: "variables.env" });
+dotenv.config({ path: 'variables.env' });
 
 export const signUp = async (req, res) => {
+  console.log(req.body);
+
   const { email, password, name } = req.body;
   const findUSer = await User.findOne({ email });
 
   if (findUSer) {
-    res.status(400).json({ status: false, data: "El usuario ya existe" });
+    res.status(400).json({ status: false, data: 'El usuario ya existe' });
   }
 
   const salt = await bcrypt.genSalt(10);
@@ -26,22 +28,23 @@ export const signUp = async (req, res) => {
 };
 
 export const login = async (req, res) => {
+  console.log(req.body);
   const { email, password } = req.body;
 
   try {
     const findUser = await User.findOne({ email });
 
     if (!findUser) {
-      res.json("El usuario no existe");
+      res.json('El usuario no existe');
     }
 
     const verifyPass = await bcrypt.compare(password, findUser.password);
 
     if (!verifyPass) {
-      res.json("El email o el password no son correctos");
+      res.json('El email o el password no son correctos');
     }
 
-    const token = createToken(findUser, process.env.SECRET, "48h");
+    const token = createToken(findUser, process.env.SECRET, '48h');
 
     res.status(200).json({ token });
   } catch (error) {
